31df4333e10ba7092aafa9f2099547e621377e64
GAK00 <geran.kunz@gmail.com>
2016-10-26 08:41:30 -0600

    Added constructor and picture

--- src/chat/view/ChatViewer.java
+++ src/chat/view/ChatViewer.java
@@ -1,8 +1,21 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class ChatViewer {
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+		public ChatViewer(){
+			windowMessage = "This message brought to you by the chatbot!";
+			chatIcon = new ImageIcon(getClass().getResource("images/chatbot.png"));
+		}
+	/**
+	 * Creates an input popup to collect response from user as a string.
+	 * @param question The question to ask the user.
+	 * @return The users response.
+	 */
 	public String collectResponse(String question)
 	{
 	String response = "";
@@ -10,12 +23,19 @@ public class ChatViewer {
 	return response;
 		
 	}
-	
+	/**
+	 * Make a popup window to display the supplied message.
+	 * @param message The message to be placed in the popup.
+	 */
 	public void diplayMessage(String message)
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
-	
+	/**
+	 * Creates a popup to ask a yes/no/cancel type question.
+	 * @param question The question being asked to the user.
+	 * @return The constant value from JOptionPane for yes/no/cancel.
+	 */
 	public int collectuserOption(String question){
 		int response = 0;
 		response = JOptionPane.showConfirmDialog(null, question);
